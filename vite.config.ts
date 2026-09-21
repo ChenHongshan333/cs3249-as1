4e964f0fb4fcd55bfa8144410ba4f76982938e0d
ChenHongshan333 <e1471281@u.nus.edu>
2026-09-21 20:58:50 +0800

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ command, mode }) => ({
+  base: '/cs3249-as1/',
   plugins: [react({
     babel: command === 'serve' && mode !== 'production'
       ? { plugins: [['module:@locator/babel-jsx', { env: 'development' }]] }
